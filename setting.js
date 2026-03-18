0d0795c63893e827aba0ed7d9e88c06de4686997
mynameiszain86-coder <mynameiszain86@gmail.com>
2026-03-19 00:51:52 +0500

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,13 +1,13 @@
-//=====💫 sᴛᴀʀ  xᴅ🔥====
+//=====💫 sᴛᴀʀ  zain🔥====
 
 module.exports = {
-  BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263714757857', // add your phone number
+  BOT_NAME: 'king of dewan sahab', // ur bot name
+  OWNER_NAME: 'zain hacker', //your name
+  OWNER_NUMBER: '923155579359', // add your phone number
   PREFIXES: ['.', '!', '#'], // Multiple prefix support you can add one or more
   
   
-  SESSION_ID: '', // add your session ID
+  SESSION_ID: '', // king~LQ8r4f
   PAIRING_NUMBER: '', // In Case if you don't have session id get pairing code by putting your number here
   TIMEZONE: 'Africa/Harare' // ur timezone
 };
